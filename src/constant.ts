0ef20fcba3250c16b48d63f374adcc893c4d915e
jovicheng <jovicheng@foxmail.com>
2023-11-17 22:50:02 +0800

    [fix] url

--- src/constant.ts
+++ src/constant.ts
@@ -1,7 +1,7 @@
 export const FULLNODE_URL = "https://fullnode.devnet.sui.io";
 
 export const CLIENT_ID =
-  "573120070871-0k7ga6ns7∏9ie0jpg1ei6ip5vje2ostt6.apps.googleusercontent.com";
+  "573120070871-0k7ga6ns79ie0jpg1ei6ip5vje2ostt6.apps.googleusercontent.com";
 
 export const REDIRECT_URI = "https://sui-zklogin.vercel.app/";
 
